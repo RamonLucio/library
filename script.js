60214325b78cdb9ef6b59a9c55c4a7fd85834718
Ramon <ramonluciolinhares@gmail.com>
2020-09-02 08:58:38 -0300

    Write a function that loops through the array and displays each book on the page

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let bookshelfArray = [{title: '1984', author: 'George Orwell', pages: 328, read: 'Read'}];
+const container = document.querySelector('#container')
 
+// store book objects
+let libraryArray = [{title: '1984', author: 'George Orwell', pages: 328, read: 'Read'}];
+
+// the constructor function
 function Book(title, author, pages, read) {
     this.title = title,
     this.author = author,
@@ -7,6 +11,16 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
+// store new book objects into libraryArray
 function addBookToLibrary(title, author, pages, read) {
-    bookshelfArray.push(new Book(title, author, pages, read))
+    libraryArray.push(new Book(title, author, pages, read))
 }
+
+// loops through the array and displays each book on the page
+function displayLibraryArray() {
+    libraryArray.forEach((obj) => {
+        let div = document.createElement('div');
+        div.innerHTML = `<p>${obj.title}</p><p>${obj.author}</p><p>${obj.pages} pages</p><p>${obj.read}</p>`;
+        container.appendChild(div);
+    });
+};
