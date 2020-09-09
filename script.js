a4f6b441ca214519ba0366c6bdfa2d9f18dc29ba
Ramon <ramonluciolinhares@gmail.com>
2020-09-09 07:12:29 -0300

    Add a button that brings up a form to input new books

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const container = document.querySelector('#container')
-
 // store book objects into array
 let library = [{title: '1984', 
 author: 'George Orwell', 
@@ -14,14 +12,24 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
+const submit = document.querySelector('#submit-button');
+submit.addEventListener('click', addBookToLibrary);
 // append new book objects into library
-function addBookToLibrary(title, author, pages, read) {
-    library.push(new Book(title, author, pages, read))
+function addBookToLibrary() {
+    let newTitle = document.getElementById("title").value;
+    let newAuthor = document.getElementById("author").value;
+    let newPages = document.getElementById("pages").value;
+    let newRead = document.getElementById("read").value;
+    library.push(new Book(newTitle, newAuthor, newPages, newRead));
+    document.getElementById("form-popup").style.display = "none";
+    displayLibrary(library);
 }
 
+const container = document.querySelector('#container');
 // loops through the array and displays each book on the page
-function displayLibrary(library) {
-    library.forEach((book) => {
+function displayLibrary(arr) {
+    container.innerHTML = '';
+    arr.forEach((book) => {
         let div = document.createElement('div');
         div.innerHTML = `<h3>${book.title}</h3><p>${book.author}</p><p>${book.pages}
          pages</p><p>${book.read}</p>`; 
