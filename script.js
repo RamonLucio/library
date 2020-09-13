fe4b66eaad5588fa82d5eaaba5c5834467e64070
Ramon <ramonluciolinhares@gmail.com>
2020-09-13 08:01:48 -0300

    Add a function on each book’s display to change its read status

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
+/* create the function that toggles a book’s read status
+on Book prototype instance.*/
+Book.prototype.toggleReadValue = function() {
+    return this.read == 'Read'? this.read = 'Unread' : this.read = 'Read' 
+}
+
 const submit = document.querySelector('#submit-button');
 submit.addEventListener('click', addBookToLibrary);
 // append new book objects into library
@@ -16,7 +22,7 @@ function addBookToLibrary() {
     let newTitle = document.getElementById("title").value;
     let newAuthor = document.getElementById("author").value;
     let newPages = document.getElementById("pages").value;
-    let newRead = document.getElementById("read").value;
+    let newRead = document.querySelector('input[name="status"]:checked').value;
     library.push(new Book(newTitle, newAuthor, newPages, newRead));
     document.getElementById("form-popup").style.display = "none";
     displayLibrary(library);
@@ -29,19 +35,33 @@ function displayLibrary(arr) {
     arr.forEach((book) => {
         let div = document.createElement('div');
         div.dataset.index = library.indexOf(book);
-        div.innerHTML = `<h3>${book.title}</h3><p>${book.author}</p><p>${book.pages}
-         pages</p><p>${book.read}</p>`;
-        // Add a button on each book’s display to remove the book from the library
-        let removeButton = document.createElement('button');
-        removeButton.textContent = 'Remove';
-        div.appendChild(removeButton);
-        container.appendChild(div);
-        removeButton.addEventListener('click', function() {
-            library.splice(div.index, 1);
-            removeButton.parentElement.remove();
-        })
+        div.innerHTML = `<h3>${book.title}</h3>
+        <p>${book.author}</p>
+        <p>${book.pages}pages</p>
+        <button onclick="toggleReadStatus(${div.dataset.index})">
+        ${book.read}</button>`;
+        addRemoveButton(div);
     });
-};
+}
+
+// Add a function on each book’s display to change its read status. 
+function toggleReadStatus(index) {
+    library[index].toggleReadValue()
+    let readButton = document.querySelector(`[data-index="${index}"] button`)
+    readButton.textContent = `${library[index].read}`
+}
+
+// Add a button on each book’s display to remove the book from the library
+function addRemoveButton(div) {
+    let removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    div.appendChild(removeButton);
+    container.appendChild(div);
+    removeButton.addEventListener('click', function() {
+        library.splice(div.index, 1);
+        removeButton.parentElement.remove();
+    })
+}
 
 function openForm() {
     document.getElementById("form-popup").style.display = "block";
